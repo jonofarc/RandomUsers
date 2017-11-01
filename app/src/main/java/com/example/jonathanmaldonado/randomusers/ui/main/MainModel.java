c2e6dbb30f2395a157f7bccf9b0d2138dece6931
Jonathan Maldonado <jmaldonadom1989@gmail.com>
2017-10-31 20:26:01 -0400

    Add pojos

--- app/src/main/java/com/example/jonathanmaldonado/randomusers/ui/main/MainModel.java
+++ app/src/main/java/com/example/jonathanmaldonado/randomusers/ui/main/MainModel.java
@@ -1,10 +1,16 @@
 package com.example.jonathanmaldonado.randomusers.ui.main;
 
+import retrofit2.Retrofit;
+
 /**
  * Created by Jonathan Maldonado on 10/31/2017.
  */
 
 public class MainModel {
+
+    final static private String TAG= MainActivity.class.getSimpleName()+"_TAG";
+    private String BASE_URL="https://randomuser.me/api/";
+
 
     public String getUserData(){
         return "Rettriving Data...";
